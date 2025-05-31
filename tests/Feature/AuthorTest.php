33f153d07f3ae959a14b47b0ed1a42c4b799a0c5
kuroneko52 <cruel.wonderland@outlook.com>
2025-06-01 05:42:47 +0900

    create new tests and delete old test

---
+++ tests/Feature/AuthorTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+use PHPUnit\Framework\Attributes\Test;
+use App\Models\Author;
+use App\Helpers\LogHelper;
+
+class AuthorTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_can_create_an_author()
+    {
+        $authorData = Author::factory()->create()->make()->toArray();
+
+        $response = $this->post('/authors', $authorData);
+
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('authors', $authorData);
+    }
+
+    #[Test]
+    public function it_can_read_authors()
+    {
+        $author = Author::factory()->create();
+
+        $response = $this->get('/authors');
+
+        $response->assertStatus(200);
+        $response->assertSee($author->name);
+    }
+
+    #[Test]
+    public function it_can_update_an_authors()
+    {
+        $author = Author::factory()->create();
+
+        $updatedData = [
+            'name' => 'DAZAI OSAMU'
+        ];
+
+        $response = $this->put("/authors/{$author->id}", $updatedData);
+
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('authors', $updatedData);
+    }
+
+    #[Test]
+    public function it_can_delete_an_authors()
+    {
+        $author = Author::factory()->create();
+
+        $response = $this->delete("/authors/{$author->id}");
+
+        $response->assertStatus(302);
+        $this->assertDatabaseMissing('authors', [
+            'id' => $author->id,
+        ]);
+    }
+
+}
